ba76c6f227385b0c7830d168bf9e84cc38e84337
yanxi.zy <bug2bug@126.com>
2018-01-06 23:44:51 +0800

    first commit

---
+++ spider-app/src/main/java/com/wawa/spiderapp/SpiderAppApplication.java
@@ -0,0 +1,14 @@
+package com.wawa.spiderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SpiderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderAppApplication.class, args);
+	}
+}
---
+++ spider-app/src/test/java/com/wawa/spiderapp/SpiderAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wawa.spiderapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpiderAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
