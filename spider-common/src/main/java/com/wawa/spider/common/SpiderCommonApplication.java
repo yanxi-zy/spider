de624fd6440a6e8d8eafcd231b6d70d90109000e
yanxi.zy <bug2bug@126.com>
2018-01-06 23:52:39 +0800

    add spider-common

---
+++ spider-common/src/main/java/com/wawa/spider/common/SpiderCommonApplication.java
@@ -0,0 +1,12 @@
+package com.wawa.spider.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderCommonApplication.class, args);
+	}
+}
